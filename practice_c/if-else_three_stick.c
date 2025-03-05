60b7a56da8c3ad775a17d3ea5ae0742066f34426
email <md.shaquib0007@gmail.com>
2025-03-06 02:35:32 +0530

    if-else_three_stick

---
+++ practice_c/if-else_three_stick.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+
+    int a,b,c;
+    scanf("%d %d %d",&a, &b, &c);
+    
+    if(a+b>c && b+c>a && a+c>b){
+        if(a==b && b==c && c==a){
+           printf("Lengths: [%d, %d, %d]\n",a,b,c);
+           printf("Type: Equilateral\n");
+           printf("All sides are of the same length.\n");
+        }
+        else if( a==b || b==c || c==a  ){
+            printf("Lengths: [%d, %d, %d]\n",a,b,c);
+            printf("Type: Isosceles\n");
+            printf("Exactly two sides are equal.\n");
+        }
+        else if(a!=b && b!=c && c!=a){
+            printf("Lengths: [%d, %d, %d]\n",a,b,c);
+            printf("Type: Scalene\n");
+            printf("All sides are of different lengths.\n");
+        }
+    }
+    else{
+            printf("Lengths: [%d, %d, %d]\n",a,b,c);
+            printf("Type: Invalid\n");
+            printf("Cannot form a valid shape.\n");
+    }
+    
+    
+    
+    return 0;
+}
